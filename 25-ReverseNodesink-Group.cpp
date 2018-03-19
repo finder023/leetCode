c6bb0195103400a6f6659230f35a151b6e22330e
finder023 <finder023@users.noreply.github.com>
2018-03-19 20:20:00 +0800

    Create 25-ReverseNodesink-Group.cpp

---
+++ 25-ReverseNodesink-Group.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	ListNode* reverseKGroup(ListNode* head, int k) {
+		vector<ListNode*> nodes(k, NULL);
+		ListNode* tag = new ListNode(0);
+        tag->next = head;
+		ListNode* pre = tag;
+		ListNode* node = head;
+		ListNode* next;
+		bool isReversed = false;
+		while (node != NULL) {
+			//判断是否足够k个节点
+			for (int i = 0; i<k; ++i) {
+				if (node != NULL) {
+					nodes[i] = node;
+					node = node->next;
+				}
+				else {
+					isReversed = true;
+					break;
+				}
+			}
+			if (isReversed) break;
+			next = node;
+			//对k个节点进行反转
+			node = pre;
+			for (int i = k - 1; i >= 0; --i) {
+				node->next = nodes[i];
+				node = node->next;
+			}
+			node->next = next;
+			pre = node;
+			node = pre->next;
+		}
+		return tag->next;
+	}
+
+};
