55862d1911d1ca25349ca6a1d8e79a5fd2d8bacb
finder023 <finder023@users.noreply.github.com>
2018-04-03 20:51:11 +0800

    Create 41-FirstMissingPositive.cpp

---
+++ 41-FirstMissingPositive.cpp
@@ -0,0 +1,18 @@
+//这个要求感觉是不可完成的，O(n)的复杂度和常数空间
+//只允许遍历一遍就找出来
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        vector<bool> flag(nums.size(), 0);
+        int i;
+        for(i=0; i<nums.size(); ++i){
+            if(nums[i] > 0)
+                flag[nums[i]] = 1;
+        }
+        i = 1;
+        while(flag[i]) ++i;
+        return i;
+    }
+};
+
+//这个实现用的空间复杂度不合格
