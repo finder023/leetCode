32e8e90940ac362b27af1bc3ea36cd787419a584
finder023 <finder023@users.noreply.github.com>
2018-04-03 21:25:59 +0800

    Update 41-FirstMissingPositive.cpp

--- 41-FirstMissingPositive.cpp
+++ 41-FirstMissingPositive.cpp
@@ -16,3 +16,24 @@ public:
 };
 
 //这个实现空间复杂度不符合要求
+
+//满足要求的思路就是交换，直接将遍历到的数交换到合适的位置即可
+class Solution {
+public:
+	int firstMissingPositive(vector<int>& nums) {
+		int i;
+		for (i = 0; i<nums.size(); ) {
+            //这个判断的条件很重要，缺一不可
+			if (nums[i] > 0 && nums[i] < nums.size() && nums[i]-1 != i && nums[nums[i] - 1] != nums[i]) {
+				int temp = nums[nums[i]-1];
+				nums[nums[i]-1] = nums[i];
+				nums[i] = temp;
+			}
+			else
+				++i;
+		}
+		i = 0;
+		while ( i < nums.size() && nums[i]-1 == i) ++i;
+		return i+1;
+	}
+};
