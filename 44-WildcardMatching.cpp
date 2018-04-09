391c89a8285a8161d9ee2f78e572ee6e7eb8e34d
finder023 <finder023@users.noreply.github.com>
2018-04-09 15:32:50 +0800

    Update 44-WildcardMatching.cpp

--- 44-WildcardMatching.cpp
+++ 44-WildcardMatching.cpp
@@ -23,3 +23,29 @@ public:
 //递归还是一种试探性的算法，所有的情况都试了一遍，如果是true的话匹配到就可以结束，但是如果是false的话需要遍历所有情况，这要耗费很多的时间，以上程序不出所料，超时了。。。。。。
 
 //所以，试试动态规划的方法吧
+
+bool isMatch(const char *s, const char *p) {
+        const char* star=NULL;
+        const char* ss=s;
+        while (*s){
+            //advancing both pointers when (both characters match) or ('?' found in pattern)
+            //note that *p will not advance beyond its length 
+            if ((*p=='?')||(*p==*s)){s++;p++;continue;} 
+
+            // * found in pattern, track index of *, only advancing pattern pointer 
+            if (*p=='*'){star=p++; ss=s;continue;} 
+
+            //current characters didn't match, last pattern pointer was *, current pattern pointer is not *
+            //only advancing pattern pointer
+            if (star){ p = star+1; s=++ss;continue;} 
+
+           //current pattern pointer is not star, last patter pointer was not *
+           //characters do not match
+            return false;
+        }
+
+       //check for remaining characters in pattern
+        while (*p=='*'){p++;}
+
+        return !*p;  
+    }
