dd9e2bbe9da444b5d2c797de3ba9e55da84c8a94
finder023 <finder023@users.noreply.github.com>
2018-04-09 17:04:04 +0800

    Update 45-JumpGameII.cpp

--- 45-JumpGameII.cpp
+++ 45-JumpGameII.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 	int jump(vector<int>& nums) {
-		vector<int> mem(nums.size(), INT_MAX);
+	vector<int> mem(nums.size(), INT_MAX);
         mem[mem.size()-1] =0;
         int templen = 0;
         for(int i=nums.size()-1; i>=0; --i){   
@@ -18,3 +18,23 @@ public:
 };
 
 //动态规划算法都超时了。。。。。。
+class Solution {
+public:
+	int jump(vector<int>& A) {
+		int n = A.size();
+		if (n<2)return 0;
+		int level = 0, currentMax = 0, i = 0, nextMax = 0;
+
+		while (currentMax - i + 1>0) {		//nodes count of current level>0
+			level++;
+			for (; i <= currentMax; i++) {	//traverse current level , and update the max reach of next level
+				nextMax = max(nextMax, A[i] + i);
+				if (nextMax >= n - 1)return level;   // if last element is in level+1,  then the min jump=level 
+			}
+			currentMax = nextMax;
+		}
+		return 0;
+	}
+
+};
+//还是先前的那种贪心思路，不过方法上改进了一下
