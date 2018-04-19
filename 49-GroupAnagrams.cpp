c37f8d1ff79c033c3b9610fe37615e38c7a2115c
finder023 <finder023@users.noreply.github.com>
2018-04-19 21:23:30 +0800

    Create 49-GroupAnagrams.cpp

---
+++ 49-GroupAnagrams.cpp
@@ -0,0 +1,33 @@
+//就是所谓的变位词
+
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<string> sortedString(strs.begin(), strs.end());
+        for(int i=0; i<sortedString.size(); ++i){
+            sort(sortedString[i].begin(), sortedString[i].end());
+        }
+        vector< vector<string> > result;
+        
+        unordered_map<string, int> msi;
+        unordered_map<string, int>::iterator it;
+        int index = 0;
+        for(int i=0; i<sortedString.size(); ++i){
+            if((it = msi.find(sortedString[i])) != msi.end()){
+                result[it->second].push_back(strs[i]);
+            }
+            else{
+                vector<string> temp;
+                temp.push_back(strs[i]);
+                result.push_back(temp);
+                msi.insert(pair<string, int>(sortedString[i], index++));
+            }
+        }
+        return result;
+    }
+};
