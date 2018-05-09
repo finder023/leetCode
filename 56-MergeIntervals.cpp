0b5df2754709a7ff9fd526f3640440bdbfe43c95
finder023 <finder023@users.noreply.github.com>
2018-05-09 11:07:00 +0800

    Create 56-MergeIntervals.cpp

---
+++ 56-MergeIntervals.cpp
@@ -0,0 +1,39 @@
+//最近写的东西脑子抽风，运行时间疯狂爆炸
+
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    vector<Interval> merge(vector<Interval>& intervals) {
+        list<Interval>::iterator it, st;
+        list<Interval> lint;
+        for(int i=0; i<intervals.size(); ++i)
+            lint.push_back(intervals[i]);
+        for(it = lint.begin(); it!=lint.end(); ++it){
+            for(st = lint.begin(); st != lint.end(); ){
+                if(it == st){ ++st; continue;} 
+                if((it->start >= st->start && it->start <= st->end) || (it->end >= st->start && it->end <= st->end)){
+                    it->start = min(st->start, it->start);
+                    it->end = max(st->end, it->end);
+                    st = lint.erase(st);
+                    continue;
+                }
+                ++st;
+            }
+        }
+        vector<Interval> result;
+        for(it=lint.begin(); it!=lint.end(); ++it)
+            result.push_back(*it);
+        if(result.size() != intervals.size())
+            return merge(result);
+        return result;
+    }
+   
+};
